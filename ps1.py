0d03aca25a172f0507de6f828cebee3e479664bf
Noah Stephen Haskell <nstephenh@gmail.com>
2015-09-28 16:23:47 -0400

    Answered more questions

--- ps1.py
+++ ps1.py
@@ -18,7 +18,7 @@ def yearify(yearnoformat):
 	"""Precondition: yearnoformat is a 2 or 4 letter string containing a year
 	Postcondition: Return the year as a 4 letter string
 
-"""
+"""	
 	if len(yearnoformat) == 2 :
 		if int(yearnoformat) < 15:
 			return "20" + yearnoformat
@@ -49,7 +49,7 @@ dd-mm-yy
 """
 	if theDate[2] == "-" or theDate [2] == "/":
 		return int(theDate[0:2]) + (int(theDate[3:5]) ) + int(yearify(theDate[6:]))
-	else:
+	else:	
 		return int(theDate[0:2]) + int(theDate[2:4]) + int(yearify(theDate[4:]))
 
 #here are some tests to implement.
@@ -80,31 +80,54 @@ postc: returns True if the year leaps.
 def dayInYear(year, month, day):
 	"""prec:  year/month/day is a valid date
 	postc: returns the ordinal position of the day in the year
-	(Feb 15 is the 44th day of year 2000).
+	(Feb 15 is the 46th day of year 2000).
 	Hint:  sum is your friend"""
 	isleap = int(isLeap(year))
-	dayspermonth = (31, (isleap + 28), 31, 30 , 31, 30 , 31, 31, 30, 31, 30 , 31)
+	dayspermonth = [31, (isleap + 28), 31, 30 , 31, 30 , 31, 31, 30, 31, 30 , 31]
+	return (sum(dayspermonth[:month-1]) +day)
 
 def daysLeftInYear(year, month, day):
-    """prec:  year/month/day is a valid date
-    postc: returns the number of days left in the year
-    (Feb 15 is the 44th day of year 2000)."""
-    return 0
+	"""prec:  year/month/day is a valid date
+	postc: returns the number of days left in the year
+	(Feb 15 is the 46th day of year 2000)."""
+	lengthOfYear = 365
+	if isLeap(year):
+		lengthOfYear +=1
+	return (lengthOfYear - dayInYear(year, month, day))
 
 def daysToGraduation(month, day):
-    """prec:  year/month/ is a valid date in 2015 before graduation
-    postc: returns the number of days until graduation
-"""
-    return 0
+	"""prec:  year/month/ is a valid date in 2016 before graduation
+	postc: returns the number of days until graduation
+	"""
+	#may 28, 2016
+	return dayInYear(2016, 5, 28) - dayInYear(2016, month, day)
+	
 
 def dhms(secs):
-    """prec:  secs is a nonnegative integer
-    postc: return a STRING of the form d:hh:mm:ss, where
-    d is the number of days, h is the number of hours, m is the number     of minutes, and s is the number of seconds, h < 24, 0 <= m, s, < 60.
+	"""prec:  secs is a nonnegative integer
+postc: return a STRING of the form d:hh:mm:ss, where
+d is the number of days, h is the number of hours, m is the number     of minutes, and s is the number of seconds, h < 24, 0 <= m, s, < 60.
 Give h, m, s a two character width, padding with zeroes as needed.
 """
-    return ""
-
+	d, h, m, s = "0","0","0","0" 
+	if secs/(60*60*24) >= 1:
+		d = str(int(secs/(60*60*24)))
+		secs = secs % (60*60*24)
+	if secs/3600 >= 1:
+		h = str(int(secs/3600))
+		secs = secs % 3600
+		
+		if len(h) == 1:
+			h = "0" + h
+	if secs/60 >= 1:
+		m = str(int(secs/60))
+		secs = secs % 3600
+		if len(m) == 1:
+			m = "0" + m
+		s = str(secs)
+		if len(s) == 1:
+			s = "0" + m
+	return ( d + ":" + h + ":"+ m + ":" + s )
 
 
 def waterCloset(theString):
@@ -147,9 +170,26 @@ if __name__ == "__main__":
 						d = str(d)
 						if len (d) == 1:
 							d = "0" + d
-						cases= []
+						cases = []
 						for seperator in ["", "/", "-"]:
 							cases.append(m + seperator + d + seperator + yfour)
 						for case in cases: # and each way of writing that month
 							if (int(m) + int(d) + int(yfour)) != datePlus(case): #check and see if the sum is right
 								print ("Failure for case " + case)
+						cases = []
+						for seperator in ['', '/', '-']:
+							cases.append(m + seperator + d + seperator + str(ytwo))
+						for case in cases:
+							if (int(m) + int(d) + int(yearify(ytwo))) != datePlus(case):
+								print ("Failure for case " + case)
+
+
+	#code to test daysinyear
+	print("dayInYear(2000,02,15) == 44 returns " + str(dayInYear(2000, 2, 15) == 46))
+	#code to test daysLeftInYear
+	print("dayLeftInYear(2000, 8, 21) == 132 returns " + str(daysLeftInYear(2000, 8, 21) == 132))
+	#code to test daysTillGraduation
+	print("daysToGraduation(5, 23) == 5 returns " + str(daysToGraduation(5, 23) == 5))
+	# code to test dhms
+	print (dhms(273602)) #3:04:00:02
+	print (dhms(311023)) #3:14:23:43
